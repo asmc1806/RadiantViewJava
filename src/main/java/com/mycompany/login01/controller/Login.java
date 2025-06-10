b2af8ed0581b2b786843b152a02f326b4779d80a
asmc1806 <a-cmoreno@hotmail.com>
2025-06-10 11:05:46 -0500

    Proyecto base - login base

---
+++ src/main/java/com/mycompany/login01/controller/Login.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.mycompany.login01.controller;
+
+import com.sun.org.apache.bcel.internal.generic.AALOAD;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
+
+/**
+ *
+ * @author cb00270
+ */
+@Named(value = "login")
+@SessionScoped
+public class Login implements Serializable {
+
+    private String usuario;
+    private String contrasena;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+    public String iniciarSesion(){
+        if (usuario.equals("admin") && contrasena.equals("Clave123")) {
+            return "inicio";
+        }else{
+            FacesContext fc = FacesContext.getCurrentInstance();
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario y/o Contraseña  incorrectos", "MSG_INFO");
+            fc.addMessage(null, fm);
+            return null;
+        }
+    }
+    
+    public Login() {
+    }
+}
