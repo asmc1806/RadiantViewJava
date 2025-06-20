95d8cbedb7cd66f40dc27527394a948d00721b55
asmc1806 <a-cmoreno@hotmail.com>
2025-06-20 18:56:50 -0500

    Proyecto-Estilos

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -1,5 +1,49 @@
 $(document).ready(function () {
-  $('#sidebarCollapse').on('click', function () {
+    $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
-  });
+    $('body').toggleClass('sidebar-collapsed');
 });
+
+
+
+$('#t-all').DataTable({
+    dom: 'Bfrtip',
+    language: {
+        url: "https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
+    },
+    pageLength: 20,
+    buttons: [
+        {
+            extend: 'copy',
+            text: '<i class="fa fa-copy"></i> Copiar',
+            className: 'dt-button main_bt'
+        },
+        {
+            extend: 'csv',
+            text: '<i class="fa fa-file-csv"></i> CSV',
+            className: 'dt-button main_bt'
+        },
+        {
+            extend: 'excel',
+            text: '<i class="fa fa-file-excel-o"></i> Excel',
+            className: 'dt-button main_bt'
+        },
+        {
+            extend: 'pdf',
+            text: '<i class="fa fa-file-pdf-o"></i> PDF',
+            className: 'dt-button main_bt'
+        },
+        {
+            extend: 'print',
+            text: '<i class="fa fa-print"></i> Imprimir',
+            className: 'dt-button main_bt'
+        }
+    ]
+});
+    
+    
+    
+    
+});
+
+
